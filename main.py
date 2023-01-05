91a23b418894f070446efd992a5b01cf4e55288f
zee userbot <121794799+zeeuserbot@users.noreply.github.com>
2023-01-06 02:29:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ REPO = """
 
 if config.BOT_TOKEN:
     bot = Client(
-        "MusicPlayer",
+        "Music-Userbot",
         api_id=config.API_ID,
         api_hash=config.API_HASH,
         bot_token=config.BOT_TOKEN,
