cdab31feaecd6c7b7d327de6778a9ef6ba962c12
zee userbot <121794799+zeeuserbot@users.noreply.github.com>
2023-01-06 02:40:14 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 echo ">> FETCHING UPSTREAM..."
 git clone https://github.com/zeeuserbot/Music-Userbot /Music-Userbot
 echo ">> INSTALLING REQUIREMENTS..."
-cd /MusicPlayer
+cd /Music-Userbot
 pip3 install -U -r requirements.txt
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 clear
